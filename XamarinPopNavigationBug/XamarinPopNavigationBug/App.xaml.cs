5da037a71c32fab6cafc5c873921183efd4fc2bd
Keith Pickford <Keith@FunctionZero.com>
2023-03-22 16:18:47 +0000

    Add project files.

---
+++ XamarinPopNavigationBug/XamarinPopNavigationBug/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinPopNavigationBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            App.Current.PageAppearing += Current_PageAppearing;
+            App.Current.PageDisappearing += Current_PageDisappearing;
+        }
+
+        private void Current_PageDisappearing(object sender, Page e)
+        {
+            Debug.WriteLine($"Current_PageDisappearing {e}");
+        }
+
+        private void Current_PageAppearing(object sender, Page e)
+        {
+            Debug.WriteLine($"Current_PageAppearing {e}");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
